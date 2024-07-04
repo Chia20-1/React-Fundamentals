d2a9251b6fa9c0547cb7671661662dcd3d439bcd
Chia20-1 <chiachunlim97@gmail.com>
2024-07-05 01:29:24 +0800

    dynamic listing throug mapping array

--- src/App.jsx
+++ src/App.jsx
@@ -38,10 +38,15 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
+            {/* generating each component one by one could break when one data is missing. */}
+            {/* Lets generate it dynamically with array appraoch */}
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+            {/* <CoreConcept {...CORE_CONCEPTS[0]} />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
           </ul>
         </section>
         <section id="examples">
