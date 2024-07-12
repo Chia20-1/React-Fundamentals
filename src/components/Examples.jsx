6a9b80593aa7edca3d9891d9d3f11c421f9b3f13
Chia20-1 <chiachunlim97@gmail.com>
2024-07-12 22:33:03 +0800

    Slotting multiple component inside a component

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,6 +2,7 @@ import TabButton from "./TabButton";
 import Section from "./Section";
 import { useState } from "react";
 import { EXAMPLES } from "../data";
+import Tabs from "./Tabs";
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState("");
@@ -29,65 +30,46 @@ export default function Examples() {
 
   return (
     <Section id="examples" title="Examples">
-      <menu>
-        <TabButton
-          isSelected={selectedTopic === "components"}
-          onClick={() => {
-            handleSelect("components");
-          }}
-        >
-          Components
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === "jsx"}
-          onClick={() => {
-            handleSelect("jsx");
-          }}
-        >
-          JSX
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === "props"}
-          onClick={() => {
-            handleSelect("props");
-          }}
-        >
-          Props
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === "state"}
-          onClick={() => {
-            handleSelect("state");
-          }}
-        >
-          State
-        </TabButton>
-      </menu>
-      {/* Ternerary Expression Appraoch */}
-      {/* {!selectedTopic ? (
-            <p>Please select a topic</p>
-          ) : (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )} */}
-
-      {/* && Conditional */}
-      {/* {!selectedTopic && <p>Please select a topic</p>} */}
-      {/* {selectedTopic && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )} */}
-      {tabContent}
+      <Tabs
+        button={
+          <>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onClick={() => {
+                handleSelect("components");
+              }}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onClick={() => {
+                handleSelect("jsx");
+              }}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onClick={() => {
+                handleSelect("props");
+              }}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onClick={() => {
+                handleSelect("state");
+              }}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {tabContent}
+      </Tabs>
     </Section>
   );
 }
---
+++ src/components/Tabs.jsx
@@ -0,0 +1,43 @@
+export default function Tabs({ children, button }) {
+  return (
+    <>
+      <menu>{button}</menu>
+      {children}
+    </>
+  );
+}
+
+{
+  /* Ternerary Expression Appraoch */
+}
+{
+  /* {!selectedTopic ? (
+            <p>Please select a topic</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )} */
+}
+
+{
+  /* && Conditional */
+}
+{
+  /* {!selectedTopic && <p>Please select a topic</p>} */
+}
+{
+  /* {selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )} */
+}
